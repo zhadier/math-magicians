68895be1539d50aa09b2ac104f9da220d5262b9c
Zeeshan Haider <zhadier39@gmail.com>
2022-02-10 02:55:27 +0500

    alternate method

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -20,6 +20,27 @@ export default class Calculator extends React.Component {
 
   render() {
     const { total, next, operation } = this.state;
+    const keys = [
+      'AC',
+      '+/-',
+      '%',
+      '÷',
+      '7',
+      '8',
+      '9',
+      'x',
+      '4',
+      '5',
+      '6',
+      '-',
+      '1',
+      '2',
+      '3',
+      '+',
+      '0',
+      '.',
+      '=',
+    ];
     return (
       <div className="container-calculator">
         <div className="display">
@@ -27,64 +48,102 @@ export default class Calculator extends React.Component {
           {operation || ''}
           {next || ''}
         </div>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          AC
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          +/-
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          %
-        </button>
-        <button type="button" className="button-orange" onClick={this.handleClick}>
-          ÷
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          7
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          8
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          9
-        </button>
-        <button type="button" className="button-orange" onClick={this.handleClick}>
-          x
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          4
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          5
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          6
-        </button>
-        <button type="button" className="button-orange" onClick={this.handleClick}>
-          -
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          1
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          2
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          3
-        </button>
-        <button type="button" className="button-orange" onClick={this.handleClick}>
-          +
-        </button>
-        <button type="button" className="button-gray zero" onClick={this.handleClick}>
-          0
-        </button>
-        <button type="button" className="button-gray" onClick={this.handleClick}>
-          .
-        </button>
-        <button type="button" className="button-orange" onClick={this.handleClick}>
-          =
-        </button>
+        {keys.map((values) => {
+          if (
+            values === '+'
+            || values === '-'
+            || values === '='
+            || values === 'x'
+            || values === '÷'
+          ) {
+            return (
+              <button
+                type="button"
+                className="button-orange"
+                onClick={this.handleClick}
+                key={values}
+              >
+                {values}
+              </button>
+            );
+          }
+          if (values === '0') {
+            return (
+              <button
+                type="button"
+                className="button-gray zero"
+                onClick={this.handleClick}
+                key={values}
+              >
+                {values}
+              </button>
+            );
+          }
+          return (
+            <button type="button" className="button-gray" onClick={this.handleClick} key={values}>
+              {values}
+            </button>
+          );
+        })}
       </div>
     );
   }
 }
+
+/* <button type="button" className="button-gray" onClick={this.handleClick}>
+AC
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
++/-
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
+%
+</button>
+<button type="button" className="button-orange" onClick={this.handleClick}>
+÷
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
+7
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
+8
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
+9
+</button>
+<button type="button" className="button-orange" onClick={this.handleClick}>
+x
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
+4
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
+5
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
+6
+</button>
+<button type="button" className="button-orange" onClick={this.handleClick}>
+-
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
+1
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
+2
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
+3
+</button>
+<button type="button" className="button-orange" onClick={this.handleClick}>
++
+</button>
+<button type="button" className="button-gray zero" onClick={this.handleClick}>
+0
+</button>
+<button type="button" className="button-gray" onClick={this.handleClick}>
+.
+</button>
+<button type="button" className="button-orange" onClick={this.handleClick}>
+=
+</button> */
