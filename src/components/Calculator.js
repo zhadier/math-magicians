b79c727a405a285015e8722550db63be281bee2d
Zeeshan Haider <zhadier39@gmail.com>
2022-02-10 22:41:31 +0500

    fix linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,14 +5,10 @@ import './Calculator.css';
 
 const Calculator = () => {
   const [TNO, setTNO] = useState({ total: null, next: null, operation: null });
-  // const { next, setNext } = useState(null);
-  // const { operation, setOperation } = useState(null);
+
   const handleClick = (evt) => {
     setTNO((prev) => (calculate(prev,
       evt.target.textContent)));
-    // setTotal(newTotal);
-    // setNext(newNext);
-    // setOperation(newOperation);
   };
   const { total, next, operation } = TNO;
   const keys = [
@@ -76,61 +72,3 @@ const Calculator = () => {
 };
 
 export default Calculator;
-
-/* <button type="button" className="button-gray" onClick={this.handleClick}>
-AC
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-+/-
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-%
-</button>
-<button type="button" className="button-orange" onClick={this.handleClick}>
-÷
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-7
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-8
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-9
-</button>
-<button type="button" className="button-orange" onClick={this.handleClick}>
-x
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-4
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-5
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-6
-</button>
-<button type="button" className="button-orange" onClick={this.handleClick}>
--
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-1
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-2
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-3
-</button>
-<button type="button" className="button-orange" onClick={this.handleClick}>
-+
-</button>
-<button type="button" className="button-gray zero" onClick={this.handleClick}>
-0
-</button>
-<button type="button" className="button-gray" onClick={this.handleClick}>
-.
-</button>
-<button type="button" className="button-orange" onClick={this.handleClick}>
-=
-</button> */
