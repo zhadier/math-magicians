bd82103a56b3abc63112771e8ccb7557fdcfad5b
Zeeshan Haider <zhadier39@gmail.com>
2022-02-16 03:32:53 +0500

    add home pgae component

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import './Home.scss';
+
+const Home = () => (
+  <section className="home-section">
+    <h2>Welcome to our page!</h2>
+    <p>
+      Mathematics is the science that deals with the logic of shape, quantity and arrangement. Math
+      is all around us, in everything we do. It is the building block for everything in our daily
+      lives, including mobile devices, computers, software, architecture (ancient and modern), art,
+      money, engineering and even sports. Since the beginning of recorded history, mathematical
+      discovery has been at the forefront of every civilized society, and math has been used by even
+      the most primitive and earliest cultures. The need for math arose because of the increasingly
+      complex demands from societies around the world, which required more advanced mathematical
+      solutions
+      <br />
+      <br />
+      In many cultures—under the stimulus of the needs of practical pursuits, such as commerce and
+      agriculture—mathematics has developed far beyond basic counting. This growth has been greatest
+      in societies complex enough to sustain these activities and to provide leisure for
+      contemplation and the opportunity to build on the achievements of earlier mathematicians.
+    </p>
+  </section>
+);
+
+export default Home;
