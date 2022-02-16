1f7b15c522e4c339cae39be2a55457f44233ebfa
lisandroseia <lisandroseia11@gmail.com>
2022-02-16 18:54:58 -0300

    Add testing for home, calculator and quote

---
+++ src/__test__/Calculator.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Calculator from '../components/Calculator';
+import {
+  cleanup,
+  render,
+  fireEvent,
+  waitFor,
+  screen,
+} from '@testing-library/react';
+
+afterEach(() => cleanup());
+
+it('calculator renders correctly', () => {
+  const tree = renderer.create(<Calculator />).toJSON;
+
+  expect(tree).toMatchSnapshot();
+});
+
+it('an h2 should be displayed', () => {
+  render(<Calculator />);
+
+  expect(screen.getByRole('heading')).toHaveTextContent(`Let's do some math!`);
+});
+
+it('Clicking a button should update the display', () => {
+  render(<Calculator />);
+
+  fireEvent.click(screen.getByText('2'));
+
+  expect(screen.getByTestId('display')).toHaveTextContent('2');
+});
+
+it('Clicking a button should update the display', () => {
+  render(<Calculator />);
+
+  fireEvent.click(screen.getByText('2'));
+  fireEvent.click(screen.getByText('+'));
+  fireEvent.click(screen.getByText('2'));
+  fireEvent.click(screen.getByText('='));
+
+  expect(screen.getByTestId('display')).toHaveTextContent('4');
+});
---
+++ src/__test__/Home.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Home from '../components/Home';
+
+it('renders correctly', () => {
+  const tree = renderer.create(<Home />).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
