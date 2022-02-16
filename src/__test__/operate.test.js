dec60a1cf9c92b418f2e514b1a0fb3a1235c359d
lisandroseia <lisandroseia11@gmail.com>
2022-02-16 18:00:32 -0300

    Add testing for operate.js

---
+++ src/__test__/operate.test.js
@@ -0,0 +1,64 @@
+import operate from '../logic/operate';
+
+test('expect 1 + 1 to be 2', () => {
+  // arrange
+  const operation = '+';
+  // act
+  const result = operate('1', '1', operation);
+  // assert
+  expect(result).toBe('2');
+});
+
+test('expect 2 - 1 to be 1', () => {
+  // arrange
+  const operation = '-';
+  // act
+  const result = operate('2', '1', operation);
+  // assert
+  expect(result).toBe('1');
+});
+
+test('expect 2 x 2 to be 4', () => {
+  // arrange
+  const operation = 'x';
+  // act
+  const result = operate('2', '2', operation);
+  // assert
+  expect(result).toBe('4');
+});
+
+test('expect 4 ÷ 2 to be 2', () => {
+  // arrange
+  const operation = '÷';
+  // act
+  const result = operate('4', '2', operation);
+  // assert
+  expect(result).toBe('2');
+});
+
+test('expect 4 ÷ 0 to throw', () => {
+  // arrange
+  const operation = '÷';
+  // act
+  const result = operate('4', '0', operation);
+  // assert
+  expect(result).toThrow;
+});
+
+test('expect 3 % 2 to be 1', () => {
+  // arrange
+  const operation = '%';
+  // act
+  const result = operate('3', '2', operation);
+  // assert
+  expect(result).toBe('1');
+});
+
+test('expect 4 % 0 to throw', () => {
+  // arrange
+  const operation = '%';
+  // act
+  const result = operate('4', '0', operation);
+  // assert
+  expect(result).toThrow;
+});
